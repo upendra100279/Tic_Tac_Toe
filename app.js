cd0d073feb982440f555a41f0c74d4797e765aa1
Upendra Chauhan <upendra100279@gmail.com>
2024-07-26 16:15:54 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,106 @@
+let boxes = document.querySelectorAll(".box");
+let resetBtn = document.querySelector("#reset-btn");
+let newGameBtn = document.querySelector("#new-btn");
+let msgContainer = document.querySelector(".msg-container");
+let msg = document.querySelector("#msg");
+
+let turnO = true;
+let count = 0;
+
+const winPatterns = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
+const resetGame = () => {
+  turnO = true;
+  count = 0;
+  enableBoxes();
+  msgContainer.classList.add("hide");
+};
+
+boxes.forEach((box) => {
+  box.addEventListener("click", () => {
+    if (box.disabled) return; // Ignore click if box is already disabled
+    if (turnO) {
+      box.innerText = "O";
+      turnO = false;
+    } else {
+      box.innerText = "X";
+      turnO = true;
+    }
+    box.disabled = true;
+    box.classList.add("marked"); // Add this line for animation
+    count++;
+
+    if (checkWinner()) {
+      return;
+    }
+
+    if (count === 9) {
+      gameDraw();
+    }
+  });
+});
+
+const gameDraw = () => {
+  msg.innerText = `Game was a Draw.`;
+  msgContainer.classList.remove("hide");
+  disableBoxes();
+};
+
+const disableBoxes = () => {
+  boxes.forEach((box) => (box.disabled = true));
+};
+
+const enableBoxes = () => {
+  boxes.forEach((box) => {
+    box.disabled = false;
+    box.innerText = "";
+    box.classList.remove("marked");
+  });
+};
+
+const showWinner = (winner) => {
+  msg.innerText = `Congratulations, Winner is ${winner}`;
+  msgContainer.classList.remove("hide");
+  disableBoxes();
+};
+
+const checkWinner = () => {
+  for (let pattern of winPatterns) {
+    let [a, b, c] = pattern;
+    if (
+      boxes[a].innerText &&
+      boxes[a].innerText === boxes[b].innerText &&
+      boxes[a].innerText === boxes[c].innerText
+    ) {
+      showWinner(boxes[a].innerText);
+      return true;
+    }
+  }
+  return false; // Return false if no winner is found
+};
+
+newGameBtn.addEventListener("click", resetGame);
+resetBtn.addEventListener("click", resetGame);
+
+// Mouse Trail Effect
+document.addEventListener("mousemove", (event) => {
+  let trail = document.createElement("div");
+  trail.className = "mouse-trail";
+  document.body.appendChild(trail);
+  trail.style.left = `${event.pageX}px`;
+  trail.style.top = `${event.pageY}px`;
+
+  // Remove the trail after animation
+  trail.addEventListener("animationend", () => {
+    trail.remove();
+  });
+});
